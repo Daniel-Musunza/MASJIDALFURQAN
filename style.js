c512cdf3575fae28ac759bb2120240cab691ad37
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-11-12 10:59:25 +0300

    homepage changes

--- style.js
+++ style.js
@@ -3,3 +3,24 @@ $(document).ready(function(){
         $("#navbar").slideToggle("slow");
     });
 });
+
+
+var swiper = new Swiper(".slider", {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  var swiper = new Swiper(".tesslider", {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
